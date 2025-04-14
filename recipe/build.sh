2994dbd56f8a0e789904712369b37e8699db06cd
skupr-anaconda <79282962+skupr-anaconda@users.noreply.github.com>
2025-04-14 09:33:12 +0300

    geotiff 1.7.0: Rebuild with libtiff 4.7.0 (#12)

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,6 @@
 
 cmake -D CMAKE_PREFIX_PATH=$PREFIX \
       -D CMAKE_INSTALL_PREFIX=$PREFIX \
-      -D WITH_PROJ4=ON \
       -D WITH_ZLIB=ON \
       -D BUILD_SHARED_LIBS=ON \
       -D WITH_JPEG=ON \
